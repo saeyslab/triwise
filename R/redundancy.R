e0d9c2f2d3095d4000b27c8e28072d63bac4013a
Wouter Saelens <wouter.saelens@gmail.com>
2016-11-21 15:06:59 +0100

    integrate packagedocs

--- R/redundancy.R
+++ R/redundancy.R
@@ -1,18 +1,28 @@
+# #' @export
+# estimateRedundancy <- function(scores, gsets, Gdiffexp) {
+#   scores = scores[order(scores$qval),]
+#   filteredgsets <- sapply(gsets[scores$gsetid], function(gset) {intersect(gset, Gdiffexp)})
+#   last <- c()
+#   prevgsetids = c()
+#   sapply(scores$gsetid, function(gsetid){
+#     if (length(prevgsetids) > 0) {
+#       gsetoi = filteredgsets[[gsetid]]
+#
+#       jaccards = sapply(filteredgsets[prevgsetids], jaccard, b=gsetoi)
+#     } else {
+#       jaccards = c(0)
+#     }
+#     prevgsetids <<- c(gsetid, prevgsetids)
+#     max(jaccards)
+#   })
+# }
+
 #' @export
 estimateRedundancy <- function(scores, gsets, Gdiffexp) {
-  scores = scores[order(scores$qval),]
-  filteredgsets <- sapply(gsets[scores$gsetid], function(gset) {intersect(gset, Gdiffexp)})
-  last <- c()
-  prevgsetids = c()
-  sapply(scores$gsetid, function(gsetid){
-    if (length(prevgsetids) > 0) {
-      gsetoi = filteredgsets[[gsetid]]
+  gsets_filtered = gsets[scores$gsetid]
+  G = Reduce(union, gsets_filtered)
 
-      jaccards = sapply(filteredgsets[prevgsetids], jaccard, b=gsetoi)
-    } else {
-      jaccards = c(0)
-    }
-    prevgsetids <<- c(gsetid, prevgsetids)
-    max(jaccards)
-  })
+  redundancy = mgsa::mgsa(Gdiffexp, gsets_filtered, G)
+  redundancy = setNames(redundancy@setsResults$estimate, rownames(redundancy@setsResults))
+  redundancy[scores$gsetid]
 }
