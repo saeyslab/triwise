5336420e4a67f79b6bc70229024527e99809874a
Wouter Saelens <wouter.saelens@gmail.com>
2016-11-24 17:31:57 +0100

    fixed citation file

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #'
 #' Expression dataset from van de Laar et al. 2016
 #'
-#' This dataset contains, among others, gene expression data from the three main macrophage progenitor populations in mice
-#' Note that due to size restrictions of Bioconductor, half of non-differentially expressed genes are not included in the dataset
+#' This dataset contains, among others, gene expression data from the three main macrophage progenitor populations in mice.
+#' Note that due to size restrictions of Bioconductor, half of non-differentially expressed genes are not included in the dataset.
 #' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE76999}
 "vandelaar"
